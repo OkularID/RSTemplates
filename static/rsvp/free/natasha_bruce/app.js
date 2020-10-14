ed84a95a8d4e0d243d30cf8d322f2026365d94ba
MuhammadFikriGunawan <muhammadfikrigunawan@gmail.com>
2020-10-14 19:46:25 +0700

    new update

--- static/rsvp/free/natasha_bruce/app.js
+++ static/rsvp/free/natasha_bruce/app.js
@@ -1,328 +1,108 @@
-// Higher number = more zoom
-let scaleAmount = 0.5;
-
-function scrollZoom() {
-  const images = document.querySelectorAll("[data-scroll-zoom]");
-  let scrollPosY = 0;
-  scaleAmount = scaleAmount / 100;
-
-  const observerConfig = {
-    rootMargin: "0% 0% 0% 0%",
-    threshold: 0
-  };
-
-  // Create separate IntersectionObservers and scroll event listeners for each image so that we can individually apply the scale only if the image is visible
-  images.forEach(image => {
-    let isVisible = false;
-    const observer = new IntersectionObserver((elements, self) => {
-      elements.forEach(element => {
-        isVisible = element.isIntersecting;
-      });
-    }, observerConfig);
-
-    observer.observe(image);
-
-    // Set initial image scale on page load
-    image.style.transform = `scale(${1 + scaleAmount * percentageSeen(image)})`;
-
-    // Only fires if IntersectionObserver is intersecting
-    window.addEventListener("scroll", () => {
-      if (isVisible) {
-        scrollPosY = window.pageYOffset;
-        image.style.transform = `scale(${1 +
-          scaleAmount * percentageSeen(image)})`;
-      }
-    });
-  });
-
-  // Calculates the "percentage seen" based on when the image first enters the screen until the moment it leaves
-  // Here, we get the parent node position/height instead of the image since it's in a container that has a border, but
-  // if your container has no extra height, you can simply get the image position/height
-  function percentageSeen(element) {
-    const parent = element.parentNode;
-    const viewportHeight = window.innerHeight;
-    const scrollY = window.scrollY;
-    const elPosY = parent.getBoundingClientRect().top + scrollY;
-    const borderHeight = parseFloat(getComputedStyle(parent).getPropertyValue('border-bottom-width')) + parseFloat(getComputedStyle(element).getPropertyValue('border-top-width'));
-    const elHeight = parent.offsetHeight + borderHeight;
-
-    if (elPosY > scrollY + viewportHeight) {
-      // If we haven't reached the image yet
-      return 0;
-    } else if (elPosY + elHeight < scrollY) {
-      // If we've completely scrolled past the image
-      return 100;
-    } else {
-      // When the image is in the viewport
-      const distance = scrollY + viewportHeight - elPosY;
-      let percentage = distance / ((viewportHeight + elHeight) / 100);
-      percentage = Math.round(percentage);
-
-      return percentage;
-    }
-  }
-}
-
-scrollZoom();
-
-
-
-
-
-//  ============Galley============
-var App = (function () {
-  //=== Use Strict ===//
-  'use strict';
-  //=== Private Variables ===//
-  var gallery = $('#js-gallery');
-  //=== Gallery Object ===//
-  var Gallery = {
-    switch: function(trigger, imgContainer) {
-      var src = trigger.attr('href'),
-      thumbs = trigger.siblings(),
-      img = trigger.parent().prev().children();
-      // Add active class to thumb
-      trigger.addClass('is-active');
-      // Remove active class from thumbs
-      thumbs.each(function() {
-        if( $(this).hasClass('is-active') ) {
-          $(this).removeClass('is-active');
-        }
-      });
-      // Switch image source
-      img.attr('src', src);
-    }
-  };
-  //=== Public Methods ===//
-  function init() {
-    // Listen for clicks on anchors within gallery
-    gallery.delegate('a', 'click', function(event) {
-      var trigger = $(this);
-      var triggerData = trigger.data("gallery");
-
-      if ( triggerData === 'thumb') {
-        var imgContainer = trigger.parent().siblings();
-        Gallery.switch(trigger, imgContainer);
-      } else {
-        return;
-      }
-      event.preventDefault();
-    });
-  }
-  //=== Make Methods Public ===//
-  return {
-    init: init
-  };
-})();
-App.init();
-
-
-
-
-
-//  ============Move Down============
-$('#chevron-down, .menu a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 1500);
-});
-
-//  ============Animation============
-  $(window).scroll(function() {
-    $(".slideanim").each(function(){
-      var pos = $(this).offset().top;
-
-      var winTop = $(window).scrollTop();
-        if (pos < winTop + 600) {
-          $(this).addClass("slide");
-        }
-    });
-  });
-  $(window).scroll(function() {
-    $(".slideanim").each(function(){
-      var pos = $(this).offset().top;
-
-      var winTop = $(window).scrollTop();
-        if (pos < winTop + 600) {
-          $(this).addClass("slide");
-        }
-    });
-  });
-  $(window).scroll(function() {
-    $(".slideanimX").each(function(){
-      var pos = $(this).offset().top;
-
-      var winTop = $(window).scrollTop();
-        if (pos < winTop + 600) {
-          $(this).addClass("slideX");
-        }
-    });
-  });
-  $(window).scroll(function() {
-    $(".slideanimLeft").each(function(){
-      var pos = $(this).offset().top;
-
-      var winTop = $(window).scrollTop();
-        if (pos < winTop + 600) {
-          $(this).addClass("slideLeft");
-        }
-    });
-  });
-  $(window).scroll(function() {
-    $(".slideanimLeftTour").each(function(){
-      var pos = $(this).offset().top;
-
-      var winTop = $(window).scrollTop();
-        if (pos < winTop + 600) {
-          $(this).addClass("slideLeftTour");
-        }
-    });
-  });
-  $(window).scroll(function() {
-    $(".slideanimImage1").each(function(){
-      var pos = $(this).offset().top;
-
-      var winTop = $(window).scrollTop();
-        if (pos < winTop + 600) {
-          $(this).addClass("slideImage1");
-        }
-    });
-  });
-  $(window).scroll(function() {
-    $(".slideanimImage2").each(function(){
-      var pos = $(this).offset().top;
-
-      var winTop = $(window).scrollTop();
-        if (pos < winTop + 600) {
-          $(this).addClass("slideImage2");
-        }
-    });
-  });
-  $(window).scroll(function() {
-    $(".slideanimImage3").each(function(){
-      var pos = $(this).offset().top;
-
-      var winTop = $(window).scrollTop();
-        if (pos < winTop + 600) {
-          $(this).addClass("slideImage3");
-        }
-    });
-  });
-
-
-
-$(document).ready(function(){
-
-  $(window).scroll(function(){
-    windowTop = $(window).scrollTop();
-    $('.move-right').css({
-      'transform':'translateX('+(windowTop) * 0.1  +'px)'
-    });
-    $('.move-right-fast').css({
-      'transform':'translateX('+(windowTop) * 0.4  +'px)'
-    });
-    $('.move-left').css({
-      'transform':'translateX('+(windowTop) * -0.1 +'px)'
-    });
-    $('.move-left-fast').css({
-      'transform':'translateX('+(windowTop) * -0.7 +'px)'
-    });
-    $('.move-up').css({
-      'transform':'translateY('+(windowTop) * -0.15 +'px)'
-    });
-    $('.move-up-slow').css({
-      'transform':'translateY('+(windowTop) * -0.1 +'px)'
-    });
-    $('.move-up-fast').css({
-      'transform':'translateY('+(windowTop) * -0.4 +'px)'
-    });
-    $('.move-down').css({
-      'transform':'translateY('+(windowTop) * 0.1 +'px)'
-    });
-    $('.move-down-fast').css({
-      'transform':'translateY('+(windowTop) * 0.4 +'px)'
-    });
-  });
-});
-
-
-
-
-// ============Navbar Mobile===============
 $(document).ready(function() {
-  $('nav .fa-bars').click(function() {
+      AOS.init({
+       duration: 1200
+      });
+  $('.nav-wedding .fa-bars').click(function() {
     $(".side-nav").addClass("active");
   })
-  $('.close-div .fa-times, .side-nav li').click(function() {
+  $('.close-div .fa-times, .side-nav li.mx-4').click(function() {
     $(".side-nav").removeClass("active");
   })
-  // Add smooth scrolling to all links
-  // $("nav a").on('click', function(event) {
-  //   if (this.hash !== "") {
-  //     event.preventDefault();
-  //     var hash = this.hash;
-  //     $('html, body').animate({
-  //       scrollTop: $(hash).offset().top
-  //     }, 1000, function(){
-  //       window.location.hash = hash;
-  //     });
-  //   }
-  // });
-});
-// ============Carousel===============
-$(document).ready(function() {
-  $('.one-carousel').addClass('owl-carousel owl-theme').owlCarousel({
-    margin: 0,
-    responsiveClass: true,
+  $('.carousel-gallery').addClass('owl-carousel owl-theme').owlCarousel({
+    margin: 50,
     loop: true,
-    autoplay: true,
+    responsiveClass: true,
     responsive:{
       0: {
         items: 1,
-        navText: ["<h2 class='text-blue'><i class='fa fa-chevron-circle-left'></i></h2>","<h2 class='text-blue'><i class='fa fa-chevron-circle-right'></i></h2>"],
         dots: true,
-        nav: false
+        nav: false,
+        autoplayTimeout:2500,
+        autoplay:true
       },
       600: {
         items: 1,
-        navText: ["<h2 class='text-blue'><i class='fa fa-chevron-circle-left'></i></h2>","<h2 class='text-blue'><i class='fa fa-chevron-circle-right'></i></h2>"],
         dots: true,
         nav: false
       },
       1000: {
-        items: 1,
-        navText: ["<h2 class='text-blue'><i class='fa fa-chevron-circle-left'></i></h2>","<h2 class='text-blue'><i class='fa fa-chevron-circle-right'></i></h2>"],
+        items: 2,
         dots: true,
-        nav: false
+        nav: false,
+        stagePadding: 300
       }
     }
   })
   $('.carousel-free').addClass('owl-carousel owl-theme').owlCarousel({
-    margin: 20,
+    margin: 50,
+    loop: true,
     responsiveClass: true,
-    autoplay: true,
     responsive:{
       0: {
         items: 1,
-        dots: false,
-        loop: true,
-        navText: ["<img src='/static/rsvp/free/natasha_bruce/images/nav-carousel-07.png'>","<img src='/static/rsvp/free/natasha_bruce/images/nav-carousel-08.png'>"],
-        nav: true
+        dots: true,
+        nav: false,
       },
       600: {
         items: 1,
-        dots: false,
+        dots: true,
         nav: false
       },
       1000: {
-        items: 3,
-        dots: false,
-        nav: false
+        items: 2,
+        dots: true,
+        nav: false,
       }
     }
   })
-})
+  const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
 
+let countDown = new Date('May 19, 2020 00:00:00').getTime(),
+    x = setInterval(function() {    
+
+      let now = new Date().getTime(),
+          distance = countDown - now;
+        document.getElementById('days').innerText = ("0" + Math.floor(distance / (day))).slice(-2),
+        document.getElementById('hours').innerText = ("0" + Math.floor((distance % (day)) / (hour))).slice(-2),
+        document.getElementById('minutes').innerText = ("0" + Math.floor((distance % (hour)) / (minute))).slice(-2),
+        document.getElementById('seconds').innerText = ("0" + Math.floor((distance % (minute)) / second)).slice(-2);
+
+      //do something later when date is reached
+      //if (distance < 0) {
+      //  clearInterval(x);
+      //  'IT'S MY BIRTHDAY!;
+      //}
+
+    }, second)
+
+})
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $(".nav-wedding a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
 //// ============Input number incrementer / stepper===============
   $(".btn").on("click tap", function() {
     var $button = $(this);
@@ -349,7 +129,3 @@ $(document).ready(function() {
     $("#step-decrement").removeAttr("disabled");
     $("#step-decrement").removeAttr("aria-disabled");
   });
-
-
-
-// Sync Images
